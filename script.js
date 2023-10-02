b5d0ec932e3d40e7c54d3978acbdeca7852388e4
Imam Balda <imambalda1@gmail.com>
2023-10-02 20:17:11 +0700

    Display the input value

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-let screen = document.getElementById('screen');
+// Mendapatkan elemen-elemen yang dibutuhkan
+const screen = document.getElementById("screen");
+const equalsButton = document.querySelector(".green-btn");
+const clearButton = document.querySelector(".red-btn");
+const deleteButton = document.querySelector(".red-btn");
 const buttons = document.querySelectorAll("button");
-let screenValue = '';
-for (item of buttons) {
-    item.addEventListener('click', (e) => {
-        console.log("Clicked")
-        buttonText = e.target.innerText;
-        console.log('Button text is ', buttonText);
-        if (buttonText == 'x') {
-            buttonText = '*';
-            screenValue += buttonText;
-        }
-        else if (buttonText == '=') {
 
-        }
-    })
-}
+// Menambahkan event listener pada semua tombol
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "=") {
+      // Jika tombol "=" diklik, lakukan perhitungan matematika
+      try {
+        // Ganti operator perkalian dan pembagian
+        screen.value = eval(screen.value.replace('x', '*').replace('÷', '/'));
+      } catch (error) {
+        screen.value = "Error";
+      }
+    } else if (button.textContent === "AC") {
+      // Jika tombol "AC" diklik, hapus semua input
+      screen.value = "";
+    } else if (button.textContent === "Del") {
+      // Jika tombol "Del" diklik, hapus karakter terakhir
+      screen.value = screen.value.slice(0, -1);
+    } else {
+      // Selain itu, tambahkan teks tombol ke layar
+      screen.value += button.textContent;
+    }
+  });
+});
