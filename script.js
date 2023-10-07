c6003f79f5802818919e20e16f6b2848ca778281
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-10-07 17:20:58 +0300

    Added Complete KeyBoard

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Mendapatkan elemen-elemen yang dibutuhkan
 const screen = document.getElementById("screen");
-const equalsButton = document.querySelector(".green-btn");
-const clearButton = document.querySelector(".red-btn");
-const deleteButton = document.querySelector(".red-btn");
+const equalBtn = document.getElementById('equal-btn')
+const acBtn = document.getElementById('ac-btn')
+const delBtn = document.getElementById('del-btn')
+// const equalsButton = document.querySelector(".green-btn");
+// const clearButton = document.querySelector(".red-btn");
+// const deleteButton = document.querySelector(".red-btn");
 const buttons = document.querySelectorAll("button");
+
 
 // Menambahkan event listener pada semua tombol
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.textContent === "=") {
+  button.addEventListener("click", (event) => {
+
+    if (event.target.id == 'equal-btn') {
+      console.log("=")
       // Jika tombol "=" diklik, lakukan perhitungan matematika
       try {
         // Ganti operator perkalian dan pembagian
@@ -16,15 +22,47 @@ buttons.forEach((button) => {
       } catch (error) {
         screen.value = "Error";
       }
-    } else if (button.textContent === "AC") {
+    }
+    else if (event.target.id == 'ac-btn') {
+      console.log("ac")
       // Jika tombol "AC" diklik, hapus semua input
       screen.value = "";
-    } else if (button.textContent === "Del") {
+    }
+    else if (event.target.id == 'del-btn') {
+      console.log("del")
       // Jika tombol "Del" diklik, hapus karakter terakhir
       screen.value = screen.value.slice(0, -1);
-    } else {
+    }
+    else {
       // Selain itu, tambahkan teks tombol ke layar
       screen.value += button.textContent;
     }
   });
+});
+
+
+// Add event listeners for keyboard input
+document.addEventListener('keydown', function (event) {
+
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    equalBtn.click();
+  }
+  else if (event.key === "Backspace") {
+    delBtn.click();
+  }
+  else if (event.key === "Delete") {
+    acBtn.click()
+  }
+else if (event.key.match(/[0-9/.-/+*//]/)) {
+    //press on button with text 0-9
+    document.getElementById(`${event.key}-btn`)?.click()
+  }
+  else if (event.key.match(/[/(/)]/)) {
+    //press on button with text 0-9
+    if (event.key == "(")
+      document.getElementById(`lp-btn`)?.click()
+    else if (event.key == ")")
+      document.getElementById(`rp-btn`)?.click()
+  }
 });
